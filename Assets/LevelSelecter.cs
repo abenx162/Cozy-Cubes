1b1cddea36d47ab66f227b115b167effe1331534
abenx162 <benxuan162@gmail.com>
2022-05-25 22:22:46 +0800

    25/05/22 Level Select

--- Assets/LevelSelecter.cs
+++ Assets/LevelSelecter.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Restart : MonoBehaviour
+public class LevelSelecter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void GoToLevelSelect()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void GoToLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void GoToLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
 }

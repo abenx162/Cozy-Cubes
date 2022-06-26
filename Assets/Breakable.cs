9c6628f3477c652ffa8e88042513360886086d5a
abenx162 <benxuan162@gmail.com>
2022-06-26 20:46:45 +0800

    26/06/22 fixed Breakable so that it works with Undo

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -18,6 +18,6 @@ public class Breakable : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        transform.position = new Vector2(30, 0);
     }
 }

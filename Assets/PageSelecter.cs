dc245c2d099d3d7bed36e65edb56a71311696ab6
abenx162 <benxuan162@gmail.com>
2022-07-06 02:48:44 +0800

    06/07/22 Sliding level select screen implemented

--- Assets/PageSelecter.cs
+++ Assets/PageSelecter.cs
@@ -4,33 +4,64 @@ using UnityEngine;
 
 public class PageSelecter : MonoBehaviour
 {
-    private int currentPage;
-    private int maxPage;
-    public GameObject page;
+    public GameObject pages;
+
     // Start is called before the first frame update
     void Start()
     {
-        currentPage = 1;
-        maxPage = 2;
+        GameObject.Find("Prev Page").transform.position += new Vector3(Screen.width / 2, 0, 0);
     }
 
-    IEnumerator MovePage(Vector3 dist, float seconds)
+    IEnumerator MovePage(Vector3 start, Vector3 end, float seconds)
     {
         float t = 0f;
         while (t <= 1.0)
         {
             t += Time.deltaTime / seconds;
-            page.transform.position = Vector3.Lerp(page.transform.position, page.transform.position + dist, Mathf.SmoothStep(0f, 1f, t));
+            pages.transform.position = Vector3.Lerp(start, end, Mathf.SmoothStep(0f, 1f, t));
             yield return null;
         }
     }
 
     public void NextPage()
     {
-        if (currentPage < maxPage)
+        if (pages.GetComponent<PageCount>().currentPage < pages.GetComponent<PageCount>().maxPage)
         {
-            StartCoroutine(MovePage(new Vector3(-45, 0, 0), 1));
-            currentPage++;
+            Vector3 start = pages.transform.position;
+            Vector3 end = start + new Vector3(-Screen.width, 0, 0);
+            StartCoroutine(MovePage(start, end, 1));
+            pages.GetComponent<PageCount>().currentPage++;
+
+            if (pages.GetComponent<PageCount>().currentPage == pages.GetComponent<PageCount>().maxPage)
+            {
+                gameObject.transform.position += new Vector3(Screen.width, 0, 0);
+            }
+
+            if (pages.GetComponent<PageCount>().currentPage == 2)
+            {
+                GameObject.Find("Prev Page").transform.position -= new Vector3(Screen.width, 0, 0);
+            }
+        }
+    }
+
+    public void PrevPage()
+    {
+        if (pages.GetComponent<PageCount>().currentPage > 1)
+        {
+            Vector3 start = pages.transform.position;
+            Vector3 end = start + new Vector3(Screen.width, 0, 0);
+            StartCoroutine(MovePage(start, end, 1));
+            pages.GetComponent<PageCount>().currentPage--;
+        }
+
+        if (pages.GetComponent<PageCount>().currentPage == 1)
+        {
+            gameObject.transform.position += new Vector3(Screen.width, 0, 0);
+        }
+
+        if (pages.GetComponent<PageCount>().currentPage == pages.GetComponent<PageCount>().maxPage - 1)
+        {
+            GameObject.Find("Next Page").transform.position -= new Vector3(Screen.width, 0, 0);
         }
     }
 }
